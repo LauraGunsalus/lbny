b62b09617cc800d1c8b615e726c6cabbd05d434f
Jack Strickland <jack.strickland@ucsf.edu>
2018-09-26 18:26:00 -0700

    Addition of sam partial df to full df and more

---
+++ pickle_creation/samdf_to_fulldf.py
@@ -0,0 +1,6 @@
+#The main purpose of this script is to update and flesh out a raw extracted data frame that comes directly out of
+#the bowtie --> sam file --> raw data frame pipeline. This can be thought of as pre-processing for the following steps
+#in the pipeline that follow, namely screening and pickization.
+#Cheers.
+
+import pandas as pd
